3977f320765133e57ff712112097e21e3b56f3ba
pako1733 <f.delvallerosendo@gmail.com>
2023-03-10 23:31:25 +1000

    commit initial files

---
+++ index.js
@@ -0,0 +1,28 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+
+let count = 0
+
+
+function increment(){
+    count +=1
+    countEl.textContent = count
+    
+}
+
+
+function save(){
+    let countStr = count + " - "
+    console.log(countStr)
+    saveEl.textContent =  saveEl.textContent + countStr
+    count = 0
+    
+}
+
+let firstName = "Francisco"
+let lastName = "Del Valle"
+
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
